8fff418b4a4466094b5c1a420d7e9f44976c07ab
MisssLenox <ivanova123402@mail.ru>
2023-12-27 11:08:32 +0300

    itog

---
+++ Animals/Animal.java
@@ -0,0 +1,10 @@
+@@ -44,9 +44,5 @@ public String toString() {
+                ", dateOfBirth = '" + dateOfBirth + '\'' 
++	     
+                '}';	                
+    	    }
+
+    public static void main(String[] args) {	
+        System.out.println(new Animal("Der", "12.01.12"));	
+    }	
+}
